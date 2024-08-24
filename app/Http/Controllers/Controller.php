5f2f0659d12d1fb9e7307f10be6d93eff5f5ca3e
JakubGazdowicz <102225631+JakubGazdowicz@users.noreply.github.com>
2024-08-24 15:50:52 +0200

    instalacja l5-swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[
+    OA\Info(version: '1.0.0', title: 'TODO app Documentation'),
+    OA\Server(url: 'http://localhost', description: 'local server'),
+    OA\SecurityScheme(securityScheme: 'bearerAuth', type: 'http', name: 'Authorization', in: 'header', scheme: 'bearer'),
+]
 abstract class Controller
 {
     //
